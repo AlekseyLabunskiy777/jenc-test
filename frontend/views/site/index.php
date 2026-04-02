2b37d982eb486e34053e012e48db165244f5bf42
Алексей Лабунский <alekseylabunskiy6@gmail.com>
2026-04-02 14:25:02 +0300

    test jenk12

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,7 +8,7 @@ $this->title = 'My Yii Application';
     <div class="p-5 mb-4 bg-transparent rounded-3">
         <div class="container-fluid py-5 text-center">
             <h1>Congratulations! (Build ID: <?= time() ?>)</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.!!!</p>
+            <p class="fs-5 fw-light">You have successfully created іііyour Yii-powered application.!!!</p>
             <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
         </div>
     </div>
