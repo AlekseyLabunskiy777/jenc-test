390a283e1686333624edee347942d7abca1c05f1
Алексей Лабунский <alekseylabunskiy6@gmail.com>
2026-04-02 16:08:51 +0300

    add unit test for ExampleTest

---
+++ backend/tests/unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace backend\tests\unit;
+
+use PHPUnit\Framework\TestCase; // для PHPUnit 10+ правильно
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
